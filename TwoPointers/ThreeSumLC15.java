da64021e2efcb3448acbc3ae357811cdef3a7e4f
Chinavath Ganapathi <ganapathichinavath@gmail.com>
2026-05-26 01:01:45 +0530

    Day 3 Solved Leetcode Problem 15

---
+++ TwoPointers/ThreeSumLC15.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class ThreeSumLC15 {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for(int i = 0 ; i < nums.length-2 ; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int left = i+1, right = nums.length-1;
+
+            while(left < right){
+                int sum = nums[i]+nums[left]+nums[right];
+
+                if(sum == 0){
+                    res.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if(sum < 0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+
+        return res;
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int arr[] = new int[n];
+        
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        System.out.println(threeSum(arr));
+    }
+}
