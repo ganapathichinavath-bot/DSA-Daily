61a9252bec34e515259d80a7cee3475d188e4e92
Chinavath Ganapathi <ganapathichinavath@gmail.com>
2026-06-09 23:16:28 +0530

    Solve Second Largest Element with Optimal Solution

--- Arrays/SecondLargestElement.java
+++ Arrays/SecondLargestElement.java
@@ -26,6 +26,36 @@ class SecondLargestElement{
 }
 */
 
+/*
+// Better Approach
+import java.util.*;
+class SecondLargestElement{
+	public static int secondLargestEle(int arr[]){
+		int largest = arr[0],secondLargest = Integer.MIN_VALUE;
+		
+		for(int i : arr){
+			if(i > largest) largest = i;
+		}
+		for(int i : arr){
+			if(i > secondLargest && i != largest) secondLargest = i;
+		}
+		return secondLargest;
+	}
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int arr[] = new int[n];
+
+		for(int i = 0 ; i < n ; i++){
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(secondLargestEle(arr));
+	}
+}
+*/
+
+// Optimal Solution
 import java.util.*;
 class SecondLargestElement{
 	public static int secondLargestEle(int arr[]){
