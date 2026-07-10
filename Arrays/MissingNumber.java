6134c45a89d3c0aedcdd5947ad10490f40b14a4b
Chinavath Ganapathi <ganapathichinavath@gmail.com>
2026-07-10 18:15:50 +0530

    Solve Missing NUmber using XOR

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -1,3 +1,24 @@
+// // Using Sum Formula
+// import java.util.*;
+// class MissingNumber{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+        
+//         int n = sc.nextInt();
+        
+//         int arr[] = new int[n-1];
+//         int sum = 0;
+//         for(int i = 0 ; i < arr.length ; i++){
+//             arr[i] = sc.nextInt();
+//             sum += arr[i];
+//         }
+//         int total = n*(n+1)/2;
+        
+//         System.out.println(total-sum);
+//     }
+// }
+
+// Using XOR
 import java.util.*;
 class MissingNumber{
     public static void main(String[] args){
@@ -6,13 +27,16 @@ class MissingNumber{
         int n = sc.nextInt();
         
         int arr[] = new int[n-1];
-        int sum = 0;
+        int expected = 0;
         for(int i = 0 ; i < arr.length ; i++){
             arr[i] = sc.nextInt();
-            sum += arr[i];
+            expected ^= arr[i];
         }
-        int total = n*(n+1)/2;
+        int actual = 0;
+        for(int i = 1 ; i <= n ; i++){
+            actual ^= i;
+        }
         
-        System.out.println(total-sum);
+        System.out.println(actual^expected);
     }
 }
