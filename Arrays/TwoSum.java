4f5f7e3959a1e34f2b0f510c5be5b5c914420b68
Chinavath Ganapathi <ganapathichinavath@gmail.com>
2026-07-10 18:40:41 +0530

    Solve Two Sum Using HashMap(Optimal)

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,7 +1,42 @@
+// // Using brute force approach to find the two sum problem
+// import java.util.*;
+// class TwoSum{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+        
+//         int arr[] = new int[n];
+        
+//         for(int i = 0 ; i < arr.length ; i++){
+//             arr[i] = sc.nextInt();
+//         } 
+        
+//         int target = sc.nextInt();
+//         boolean found = false;
+//         for(int i = 0 ; i < arr.length ; i++){
+//             for(int j = i+1 ; j < arr.length ; j++){
+//                 if(arr[i]+arr[j] == target){
+//                     System.out.println("Indices are :"+i+" , "+j);
+//                     found = true;
+//                     break;
+//                 }
+//             }
+//             if(found){
+//                 break;
+//             }
+//         }
+//         if(!found){
+//             System.out.println("No pair found");
+//         }
+//     }
+// }
+
+// Using HashMap to find the two sum problem
 import java.util.*;
 class TwoSum{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        HashMap<Integer,Integer> map = new HashMap<>();
         int n = sc.nextInt();
         
         int arr[] = new int[n];
@@ -11,21 +46,19 @@ class TwoSum{
         } 
         
         int target = sc.nextInt();
-        boolean found = false;
         for(int i = 0 ; i < arr.length ; i++){
-            for(int j = i+1 ; j < arr.length ; j++){
-                if(arr[i]+arr[j] == target){
-                    System.out.println("Indices are :"+i+" , "+j);
-                    found = true;
-                    break;
-                }
+            int complement = target - arr[i];
+            
+            if(map.containsKey(complement)){
+                System.out.println(map.get(complement)+" , "+i);
+                sc.close();
+                return;
             }
-            if(found){
-                break;
-            }
+            map.put(arr[i],i);
         }
-        if(!found){
-            System.out.println("No pair found");
-        }
+        
+        System.out.println("No pair found");
+        
+        sc.close();
     }
 }
